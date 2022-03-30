f2830b52fb47f135487b81e53c166db4a69d361d
Ru <yuru0827@gmail.com>
2022-03-30 21:25:35 +0800

    sign up with google & add backend interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'products#index'
 
   resources :products,only: [:index,:show]
+
+  namespace :admin do
+    root 'products#index' #/admin
+    resources :products
+  end
 end
